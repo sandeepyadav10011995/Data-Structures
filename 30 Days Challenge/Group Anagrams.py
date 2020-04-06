9ce16454e3e1f6f50149f765bdef82af4a4f2b41
Sandeep Yadav <sandeepyadav10011995@gmail.com>
2020-04-07 00:14:40 +0530

    Group Anagrams --> Day 6

---
+++ 30 Days Challenge/Group Anagrams.py
@@ -0,0 +1,37 @@
+"""
+Given an array of strings, group anagrams together.
+
+Example:
+Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+Output:
+[
+  ["ate","eat","tea"],
+  ["nat","tan"],
+  ["bat"]
+]
+"""
+def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # Edge case
+        if len(strs) == 0:
+            return []
+        hash_map = {}
+        for s in strs:
+            sorted_str = ''.join(sorted(s))
+            # If not in hash_map
+            if sorted_str not in hash_map:
+                hash_map[sorted_str] = [s]
+            else:
+                hash_map[sorted_str].append(s)
+        return hash_map.values()
+    
+        # Edge case
+        if len(strs) == 0:
+            return []
+        # Storing the count
+        hash_map = collections.defaultdict(list)
+        for s in strs:
+            count = [0]*26
+            for c in s:
+                count[ord(c)- ord('a')] += 1
+            hash_map[tuple(count)].append(s)
+        return hash_map.values()
