233424a95516eb8f103c1ae6873d3759661110b1
Sandeep Yadav <sandeepyadav10011995@gmail.com>
2020-04-07 00:17:13 +0530

    Group Anagrams --> Day 6

--- 30 Days Challenge/Group Anagrams.py
+++ 30 Days Challenge/Group Anagrams.py
@@ -11,27 +11,29 @@ Output:
 ]
 """
 def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # Edge case
-        if len(strs) == 0:
-            return []
-        hash_map = {}
-        for s in strs:
-            sorted_str = ''.join(sorted(s))
-            # If not in hash_map
-            if sorted_str not in hash_map:
-                hash_map[sorted_str] = [s]
-            else:
-                hash_map[sorted_str].append(s)
-        return hash_map.values()
+#     # Approach 1 : Using sorted string    
+#     # Edge case
+#     if len(strs) == 0:
+#         return []
+#     hash_map = {}
+#     for s in strs:
+#         sorted_str = ''.join(sorted(s))
+#         # If not in hash_map
+#         if sorted_str not in hash_map:
+#             hash_map[sorted_str] = [s]
+#         else:
+#             hash_map[sorted_str].append(s)
+#     return hash_map.values()
     
-        # Edge case
-        if len(strs) == 0:
-            return []
-        # Storing the count
-        hash_map = collections.defaultdict(list)
-        for s in strs:
-            count = [0]*26
-            for c in s:
-                count[ord(c)- ord('a')] += 1
-            hash_map[tuple(count)].append(s)
-        return hash_map.values()
+    # Approach 2 : Storing count of the alphabet of the word. 
+    # Edge case
+    if len(strs) == 0:
+        return []
+    # Storing the count
+    hash_map = collections.defaultdict(list)
+    for s in strs:
+        count = [0]*26
+        for c in s:
+            count[ord(c)- ord('a')] += 1
+        hash_map[tuple(count)].append(s)
+    return hash_map.values()
