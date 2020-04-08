5eba4c79ac99b1bbda26355af10043012a1e4b65
Sandeep Yadav <sandeepyadav10011995@gmail.com>
2020-04-08 23:27:15 +0530

    Binary Tree Preorder Traversal --> Recursive & Iterative

--- Recursion/Binary Tree Preorder Traversal.py
+++ Recursion/Binary Tree Preorder Traversal.py
@@ -14,3 +14,40 @@ Output: [1,2,3]
 Notice : Do it both Recursively and Iteratively.
 
 """
+# Definition for a binary tree node.
+clas TreeNode:
+   def __init__(self, x):
+      self.val = x
+      self.left = None
+      self.right = None
+
+# Recursive Approach
+class Solution:
+   def preorderTraversal(self, root: TreeNode) -> List[int]:
+      output = []
+      while root:
+         output.append(root)
+         self.preorderTraversal(root.left)
+         self.preorderTraversal(root.right)
+      return output
+
+   
+# Iterative Approach
+class Solution:
+   def preorderTraversal(self, root: TreeNode) -> List[int]:
+      output = []
+      stack = [root]
+      while stack:
+         root = stack.pop()
+         output.append(root.val)
+         if root.right:
+            stack.append(root.right)
+         if root.left:
+            stack.append(root.left)
+     return output
+    
+         
+         
+         
+         
+         
