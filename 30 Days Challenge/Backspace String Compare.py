b757b404c4a9de8e89f1561761c46e1af784a04a
Sandeep Yadav <sandeepyadav10011995@gmail.com>
2020-04-09 17:50:51 +0530

    Backspace String Compare -- Day 9

---
+++ 30 Days Challenge/Backspace String Compare.py
@@ -0,0 +1,14 @@
+"""
+Given two strings S and T, return if they are equal when both are typed into empty text editors. # means a backspace character.
+
+Example 1:
+Input: S = "ab#c", T = "ad#c"
+Output: true
+Explanation: Both S and T become "ac".
+
+Example 2:
+Input: S = "ab##", T = "c#d#"
+Output: true
+Explanation: Both S and T become "".
+
+"""
