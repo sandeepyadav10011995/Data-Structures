fee229188314ba74590f0d3b5c3fbec84a9f3a26
Sandeep Yadav <sandeepyadav10011995@gmail.com>
2020-04-10 02:20:08 +0530

    Same Tree -> Recursive and Iterative Approach

--- Recursion II/Same Tree.py
+++ Recursion II/Same Tree.py
@@ -12,9 +12,46 @@ Input:     1         1
 Output: true
 
 """
-
+# Definition of the binary tree.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+        
+# Recursive Approach
 class Solution:
-
-
-
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+        if p.val != q.val:
+            return False
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+    
+# Iterative Approach
+from collections import deque
+class Solution:
+    def check(self, p: TreeNode, q: TreeNode) -> bool:
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+        if p.val != q.val:
+            return False
+        return True
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        if root is None:
+            return True
+        # Using Queue
+        queue = deque([(p, q)])
+        while queue:
+            p, q = queue.popleft()
+            if not check(p, q):
+                return False
+            if p:
+                queue.append((p.left, q.left))
+                queue.append((p.right, q.right))
+        return True
 
