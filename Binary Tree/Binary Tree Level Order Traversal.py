7e06ef135e82a481baff5ba8998af4f8fadf935d
Sandeep Yadav <sandeepyadav10011995@gmail.com>
2020-04-11 16:28:29 +0530

    Binary Tree Level Order Traversal --> Using Queue

--- Binary Tree/Binary Tree Level Order Traversal.py
+++ Binary Tree/Binary Tree Level Order Traversal.py
@@ -15,3 +15,32 @@ return its level order traversal as:
   [15,7]
 ]
 """
+
+# Definition of the binary tree.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right= None
+
+# Level Order Traversal Using Queue
+class Solution:
+    def levelOrderTraversal(self, root: TreeNode) -> List[List[int]]:
+        queue = [root]
+        output = []
+        while queue:
+            count = len(queue)
+            arr = []
+            while count:
+                temp = queue.pop(0)
+                arr.append(temp.val)
+                # Enqueue left child
+                if temp.left:
+                    queue.apppend(root.left)
+                # Enqueue right child
+                if temp.right:
+                    queue.apppend(root.right)
+                count -= 1
+            output.append(arr)
+         return output
+                
