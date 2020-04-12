2d8bd9b7af59fdf277a42595d4dc6a5321ed5f31
Sandeep Yadav <sandeepyadav10011995@gmail.com>
2020-04-13 05:15:17 +0530

    Construct Binary Tree from Inorder and Postorder Traversal --> Optimized Solution

--- Binary Tree/Construct Binary Tree from Inorder and Postorder Traversal.py
+++ Binary Tree/Construct Binary Tree from Inorder and Postorder Traversal.py
@@ -36,3 +36,24 @@ class Solution:
     def buildTree(self, inorder: List[int], postorder[int]) -> TreeNode:
         return self.helper(inorder, postorder)
     
+
+# Optimized Recursive Approach --> i.e. No Extra Space
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+class Solution:
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> TreeNode:
+        idx_map = {val: idx for idx, val in enumerate(inorder)}
+        def helper(start, end):
+            if start > end:
+                return None
+            root = TreeNode(postorder.pop())
+            root.right = helper(idx_map[root.val] + 1, end)
+            root.left = helper(start, idx_map[root.val] - 1)
+            return root
+        return helper(0, len(inorder) - 1)
+        
+        
+        
