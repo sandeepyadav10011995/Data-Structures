8c910f54add5b9b1d752b762b779d1a76dbf0d12
Sandeep Yadav <sandeepyadav10011995@gmail.com>
2020-04-18 17:38:51 +0530

    Number Of Islands --> Using BFS

--- Queue & Stacks/Number Of Islands.py
+++ Queue & Stacks/Number Of Islands.py
@@ -17,3 +17,48 @@ Input:
 00011
 Output: 3
 """
+
+class Solution:
+    def getNeighbors(self, row: int, col: int, rows: int, cols: int) -> List[(int, int)]:
+        neighbors = []
+        if row > 0:
+            neighbors.append((row-1, col))
+        if row < rows-1:
+            neighbors.append((row+1, col))
+        if col > 0:
+            neighbors.append((row, col-1))
+        if col < cols-1:
+            neighbors.append((row, col+1))
+        return neighbors
+        
+    def checkIslands(self, row: int, col: int, grid: List[List[str]], rows: int, cols: int) -> None:
+        queue = [(row, col)]
+        grid[row][col] = 0
+        while queue:
+            r, c = queue.pop(0)
+            for nr, nc in self.getNeighbors(r, c, rows, cols):
+                if grid[nr][nc] == "1":
+                    grid[nr][nc] = 0
+                    queue.append((nr, nc))
+        
+    def numIslands(self, grid: List[List[str]]) -> int:
+        rows = len(grid)
+        # Edge Case 
+        if rows == 0:
+            return 0
+        cols = len(grid[0])
+        count = 0
+        # Loop through the grid to find islands
+        for row in range(rows):
+            for col in range(cols):
+                # Check if it is island
+                if grid[row][col] == "1":
+                    # Start BFS
+                    print("start")
+                    self.checkIslands(row, col, grid, rows, cols)
+                    count += 1
+                    print(count)
+        return count
+
+
+
