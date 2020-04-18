0e626ef7a9e0861a8babc4906d9ae90fd2708e95
Sandeep Yadav <sandeepyadav10011995@gmail.com>
2020-04-18 17:59:38 +0530

    Walls And Gates --> Using BFS

--- Queue & Stacks/Walls and Gates.py
+++ Queue & Stacks/Walls and Gates.py
@@ -15,3 +15,44 @@ Output =
  [0, -1, 3, 4]]
 
 """
+
+class Solution:
+    def getNeighbors(self, row: int, col: int, rows: int, cols: int) -> List[(int)]:
+        neighbors = []
+        if row > 0:
+            neighbors.append((row-1, col))
+        if row < rows-1:
+            neighbors.append((row+1, col))
+        if col > 0:
+            neighbors.append((row, col-1))
+        if col < cols-1:
+            neighbors.append((row, col+1))
+        return neighbors
+    
+    def wallsAndGates(self, rooms: List[List[int]]) -> List[List[int]]:
+        rows = len(rooms)
+        # Edge Case
+        if rows == 0:
+            return []
+        cols = len(rooms[0])
+        
+        # Loop through the room to find the gates
+        queue = []
+        for row in range(rows):
+            for col in range(cols):
+                # Check if it is a gate
+                if rooms[row][col] == 0:
+                    queue.append((row, col))
+        # Start BFS
+        while queue:
+            r, c = queue.pop(0)
+            for nr, nc in self.getNeighbors(r, c, rows, cols):
+                if rooms[nr][nc] != INF:
+                    continue
+                grid[nr][nc] = min(grid[r][c] + 1, grid[nr][nc])
+                queue.append((nr, nc))
+        return rooms
+
+
+
+
