20836f604d65729d916854b44a457df8755de64a
Sandeep Yadav <sandeepyadav10011995@gmail.com>
2020-04-18 17:46:12 +0530

    Walls and Gates --> Using BFS

---
+++ Queue & Stacks/Walls and Gates.py
@@ -0,0 +1,17 @@
+"""
+Leet Code --> Walls and Gates Problem
+
+Example :
+Input = 
+[[INF, -1, 0, INF]
+ [INF, INF, INF, -1]
+ [INF, -1, INF, -1]
+ [0, -1, INF, INF]]
+
+Output = 
+[[3, -1, 0, 1]
+ [2, 2, 1, -1]
+ [1, -1, 2, -1]
+ [0, -1, 3, 4]]
+
+"""
