ff8156fe966216de74e7b6532dbdd0b8791f0a24
Sandeep Yadav <sandeepyadav10011995@gmail.com>
2020-04-22 23:56:16 +0530

    Subarray Sum Equals K --> Day 22

--- 30 Days Challenge/Subarray Sum Equals K.py
+++ 30 Days Challenge/Subarray Sum Equals K.py
@@ -19,6 +19,22 @@ class Solution:
                     count += 1
         return count
     
- # Note : This solution time complexity is O(n^2) --> Will lead to Time Limit Exceeded Warning
+# Note : This solution time complexity is O(n^2) --> Will lead to Time Limit Exceeded Warning
 
+# Using Hash Map --> Optimized Solution
+
+class Solution:
+    def subarraySum(nums: List[int], k: int) -> int:
+        count = 0
+        cum_sum = 0
+        hash_map = defaultdict(lambda: 0)
+        for num in nums:
+            cum_sum += num
+            if cum_sum == k:
+                count += 1
+            if (cum_sum - k) in hash_map:
+                count += hashmap[cum_sum - k]
+            hash_map[cum] += 1
+        return count
+            
     
