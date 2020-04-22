a5f8291bb1d82f9a044846746618953c493da4f1
Sandeep Yadav <sandeepyadav10011995@gmail.com>
2020-04-22 23:50:51 +0530

    Subarray Sum Equals K --> Day 22

--- 30 Days Challenge/Subarray Sum Equals K.py
+++ 30 Days Challenge/Subarray Sum Equals K.py
@@ -6,3 +6,19 @@ Input:nums = [1,1,1], k = 2
 Output: 2
 
 """
+# Brute Force Solution --> Iterating all the sub-arrays
+class Solution:
+    def subarraySum(nums: List[int], k: int) -> int:
+        N = len(nums)
+        count = 0
+        for i in range(N):
+            cum_sum = 0
+            for j in range(i, N):
+                cum_sum += nums[j]
+                if cum_sum == k:
+                    count += 1
+        return count
+    
+ # Note : This solution time complexity is O(n^2) --> Will lead to Time Limit Exceeded Warning
+
+    
