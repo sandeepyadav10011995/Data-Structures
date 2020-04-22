320603b5ed23a9895f9e222448cd3ac1f283dd6b
Sandeep Yadav <sandeepyadav10011995@gmail.com>
2020-04-22 23:45:39 +0530

    Subarray Sum Equals K -- Day 22

---
+++ 30 Days Challenge/Subarray Sum Equals K.py
@@ -0,0 +1,8 @@
+"""
+Given an array of integers and an integer k, you need to find the total number of continuous subarrays whose sum equals to k.
+
+Example 1:
+Input:nums = [1,1,1], k = 2
+Output: 2
+
+"""
