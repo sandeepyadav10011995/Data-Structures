c400df21c8fd84f23f209fc8859ed2399c016e70
Sandeep Yadav <sandeepyadav10011995@gmail.com>
2020-04-24 23:21:31 +0530

    Construct Binary Tree from Preorder and Inorder Traversal.py

---
+++ Binary Tree/Construct Binary Tree from Preorder and Inorder Traversal.py
@@ -0,0 +1,14 @@
+"""
+Given preorder and inorder traversal of a tree, construct the binary tree.
+
+Example :
+preorder = [3,9,20,15,7]
+inorder = [9,3,15,20,7]
+
+Output :
+    3
+   / \
+  9  20
+    /  \
+   15   7
+"""
