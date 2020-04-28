43989365dac5c8cf6615590293c3ec7eaff89ca3
Sandeep Yadav <sandeepyadav10011995@gmail.com>
2020-04-28 12:49:31 +0530

    Longest Common Subsequence --> Day 27

--- 30 Days Challenge/Longest Common Subsequence.py
+++ 30 Days Challenge/Longest Common Subsequence.py
@@ -23,3 +23,17 @@ Output: 0
 Explanation: There is no such common subsequence, so the result is 0.
 
 """
+# Recursive Solution
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        def lcs(s, t, m, n):
+            if m == -1 or n == -1:
+                return 0
+            elif s[m] == t[n]:
+                return 1 + lcs(s, t, m-1, n-1)
+            else:
+                return max(lcs(s, t, m-1, n), lcs(s, t, m, n-1))
+        return lcs(text1, text2, len(text1)-1, len(text2)-1)
+    
+    
+
