9ae6a6e8a074980887eb581e0f2dbfe0a2238c38
Sandeep Yadav <sandeepyadav10011995@gmail.com>
2020-05-05 21:43:50 +0530

    Topological Sort

---
+++ Graphs/Topological Sort.py
@@ -0,0 +1,9 @@
+"""
+To demonstrate that computer scientists can turn just about anything into a graph problem, let’s consider the difficult problem of 
+stirring up a batch of pancakes. The recipe is really quite simple: 1 egg, 1 cup of pancake mix, 1 tablespoon oil, and 34 cup of milk. 
+To make pancakes you must heat the griddle, mix all the ingredients together and spoon the mix onto a hot griddle. 
+When the pancakes start to bubble you turn them over and let them cook until they are golden brown on the bottom. 
+Before you eat your pancakes you are going to want to heat up some syrup. Figure 27 illustrates this process as a graph.
+
+
+"""
