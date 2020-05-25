b550bdb7aaf5cf0019c5d596fc894dc6a6b173bd
Sandeep Yadav <sandeepyadav10011995@gmail.com>
2020-05-25 15:59:36 +0530

    Update Reverse Linked List.py

--- LinkedList/Reverse Linked List.py
+++ LinkedList/Reverse Linked List.py
@@ -6,3 +6,42 @@ Input: 1->2->3->4->5->NULL
 Output: 5->4->3->2->1->NULL
 
 """
+# Recursive Approach
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+		# Base Case
+		if head is None or head.next is None:
+			return head
+		rev = self.reverseList(head.next)
+		head.next.next = head
+		head.next = None
+		return rev
+
+# Iterative Approach
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        # Iterative Approach --> Using Three pointers
+        # Edge Case
+        if head == None:
+            return None
+        cur = head
+        pre = None
+        nextNode = None
+        
+        while cur != None:
+            nextNode = cur.next
+            cur.next = pre
+            pre = cur
+            cur = nextNode
+        return pre
+#         # Iterative Approach --> Using Two pointers
+#         # Edge Case
+#         if head == None:
+#             return None
+#         cur = head
+#         pre = None
+#         while cur:
+#             cur.next, pre, cur = pre, cur, cur.next
+#         return pre
+            
+        
