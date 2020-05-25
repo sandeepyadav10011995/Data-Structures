1d2d499b1675f7b431624e3079ec21391df85faa
Sandeep Yadav <sandeepyadav10011995@gmail.com>
2020-05-25 16:01:46 +0530

    Update Remove Linked List Elements.py

--- LinkedList/Remove Linked List Elements.py
+++ LinkedList/Remove Linked List Elements.py
@@ -6,4 +6,21 @@ Input:  1->2->6->3->4->5->6, val = 6
 Output: 1->2->3->4->5
 
 """
-
+class Solution:
+    def removeElements(self, head: ListNode, val: int) -> ListNode:
+        # Edge Case
+        if not head:
+            return head
+        curr_node = head
+        prev_node = head
+        while curr_node:
+            if curr_node.val == val:
+                prev_node.next = curr_node.next
+                if head.val == val:
+                    head = head.next
+            else:
+                prev_node = curr_node
+            curr_node = curr_node.next
+        return head
+        
+		
