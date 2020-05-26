f9f3d85684a119628261c504cb79e7d8eb400558
Sandeep Yadav <sandeepyadav10011995@gmail.com>
2020-05-26 17:42:15 +0530

    Update Rotate List.py

--- LinkedList/Rotate List.py
+++ LinkedList/Rotate List.py
@@ -10,3 +10,32 @@ rotate 1 steps to the right: 5->1->2->3->4->NULL
 rotate 2 steps to the right: 4->5->1->2->3->NULL
 
 """
+class Solution:
+    def rotateRight(self, head: ListNode, k: int) -> ListNode:
+        def helper(head):
+            count = 1
+            while head.next is not None:
+                head = head.next
+                count += 1
+            return count
+        if head is None:
+            return head
+        slow = head
+        fast = head
+        ln = helper(head)
+        k = k % ln
+        if k == 0:
+            return head
+        while k > 0:
+            k -= 1
+            fast = fast.next
+
+        while fast.next is not None:
+            slow = slow.next
+            fast = fast.next
+        new_head = slow.next
+        slow.next = None
+        fast.next = head
+        return new_head
+
+	
