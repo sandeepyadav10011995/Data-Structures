1986bd6a87d7fec0c09c51583cd94371a1179488
Sandeep Yadav <sandeepyadav10011995@gmail.com>
2020-05-27 00:07:17 +0530

    Update Symmetric Tree.py

--- Binary Tree/Symmetric Tree.py
+++ Binary Tree/Symmetric Tree.py
@@ -9,3 +9,29 @@ For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
 3  4 4  3
 
 """
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        # Base Case
+        if root is None:
+            return True
+        def helper(p, q):
+            # Case 1: Leaf Node.
+			if not p and not q:
+                return True
+			# Case 2: If one of left or right child is not present.
+            if not p or not q:
+                return False
+			# Case 3: If the neighbours values are not equal.
+            if p.val != q.val:
+                return False
+            return helper(p.left, q.right) and helper(p.right, q.left)
+        return helper(root.left, root.right)
+
+
