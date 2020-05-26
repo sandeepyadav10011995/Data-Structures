f2e2bc564244c0936c915a934a92ad6dd0e5cc7e
Sandeep Yadav <sandeepyadav10011995@gmail.com>
2020-05-26 17:35:01 +0530

    Update Palindrome Linked List.py

--- LinkedList/Palindrome Linked List.py
+++ LinkedList/Palindrome Linked List.py
@@ -6,3 +6,39 @@ Input: 1->2
 Output: false
 
 """
+
+
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        def helper(head):
+            if head == None:
+                return None
+            cur = head
+            pre = None
+            while cur:
+                cur.next, pre, cur = pre, cur, cur.next
+            return pre
+        # Base Case
+        if head is None or head.next is None:
+            return True
+        
+        slow = head
+        fast = head
+        # Important Traversal --> Increase slow pointer acc to fast pointer
+        while fast is not None:
+            fast = fast.next
+            if fast is not None and fast.next is not None:
+                slow = slow.next
+                fast = fast.next
+        rev_head = helper(slow.next)
+        while head is not None and rev_head is not None:
+            if head.val == rev_head.val:
+                head = head.next
+                rev_head = rev_head.next
+                continue
+            else:
+                return False
+        return True
+	
+	
+	
