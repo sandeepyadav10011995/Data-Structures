3aad7dd80ce4c01bdf9baa3dfec6aa6eac979fdd
Sandeep Yadav <sandeepyadav10011995@gmail.com>
2020-05-31 13:27:46 +0530

    Update Populating Next Right Pointers in Each Node II.py

--- Binary Tree/Populating Next Right Pointers in Each Node II.py
+++ Binary Tree/Populating Next Right Pointers in Each Node II.py
@@ -15,3 +15,31 @@ Recursive approach is fine, you may assume implicit stack space does not count a
 
 
 """
+
+
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+# Iterative Approach
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        # Iterative Approach
+        if root is None:
+            return root
+        queue = [root]
+        while queue:
+            count = len(queue)
+            while count:
+                node = queue.pop(0)
+                if count != 1:
+                    node.next = queue[0]
+                if node.left: queue.append(node.left)
+                if node.right: queue.append(node.right)
+                count -= 1
+        return root
+	
+	
