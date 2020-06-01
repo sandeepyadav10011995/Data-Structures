56813c11a021b02579d6ec3fd7bdf4207303a87e
Sandeep Yadav <sandeepyadav10011995@gmail.com>
2020-06-02 00:03:10 +0530

    Update Convert Sorted Array to Binary Search Tree.py

--- BST/Convert Sorted Array to Binary Search Tree.py
+++ BST/Convert Sorted Array to Binary Search Tree.py
@@ -15,3 +15,26 @@ One possible answer is: [0,-3,9,-10,null,5], which represents the following heig
 
 
 """
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
+        def helper(left, right):
+            if left > right:
+                return None
+            mid = (left + right) // 2
+            print(mid)
+            
+            node = TreeNode(nums[mid])
+            node.left = helper(left, mid-1)
+            node.right = helper(mid+1, right)
+            
+            return node
+        return helper(0, len(nums)-1)
+
+
