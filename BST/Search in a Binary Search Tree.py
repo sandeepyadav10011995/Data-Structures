c55103e72f69161d4cdc57dc4308e61ba105c3a9
Sandeep Yadav <sandeepyadav10011995@gmail.com>
2020-06-01 23:57:54 +0530

    Update Search in a Binary Search Tree.py

--- BST/Search in a Binary Search Tree.py
+++ BST/Search in a Binary Search Tree.py
@@ -19,3 +19,25 @@ You should return this subtree:
 
 
 """
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def searchBST(self, root: TreeNode, val: int) -> TreeNode:
+        # Base Case
+        if root is None:
+            return None
+        while root is not None:
+            if val == root.val:
+                return root
+            if val > root.val:
+                return self.searchBST(root.right, val)
+            else:
+                return self.searchBST(root.left, val)
+
+
