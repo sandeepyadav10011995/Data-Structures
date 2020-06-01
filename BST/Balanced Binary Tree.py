10d3d5a9fed863a5831b2d92fb8c1a15404102f3
Sandeep Yadav <sandeepyadav10011995@gmail.com>
2020-06-02 00:01:32 +0530

    Update Balanced Binary Tree.py

--- BST/Balanced Binary Tree.py
+++ BST/Balanced Binary Tree.py
@@ -15,3 +15,25 @@ Return true.
 
 
 """
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def isBalanced(self, root: TreeNode) -> bool:
+        def helper(root):
+            if root is None:
+                return True, -1
+            lbalanced, lh = helper(root.left)
+            if not lbalanced:
+                return False, 0
+            rbalanced, rh = helper(root.right)
+            if not rbalanced:
+                return False, 0
+            # Final Balance Check Check
+            balance_check = abs(lh-rh) < 2
+            return balance_check, 1+max(lh, rh)
+        return helper(root)[0]
