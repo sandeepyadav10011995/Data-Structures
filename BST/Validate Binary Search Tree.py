04d4cc9cb1216edd1c890b4c82608280ff55fe28
Sandeep Yadav <sandeepyadav10011995@gmail.com>
2020-06-01 23:54:05 +0530

    Update Validate Binary Search Tree.py

--- BST/Validate Binary Search Tree.py
+++ BST/Validate Binary Search Tree.py
@@ -19,3 +19,46 @@ Output: true
 
 
 """
+
+
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def isValidBST(self, root: TreeNode) -> bool:
+        # # Recursive Approach
+        # # Edge Case
+        # if root is None:
+        #     return True
+        # # Base Case
+        # if root.left is None and root.right is None:
+        #     return True
+        # lh, rh = False, False
+        # if root.left and root.val > root.left.val:
+        #     lh = self.isValidBST(root.left)
+        # if root.right and root.val > root.right.val:
+        #     rh = self.isValidBST(root.left) 
+        # return lh + rh
+        
+        def helper(node, lower=float('-inf'), upper=float('inf')):
+            # Base Case
+            if not node:
+                return True
+            
+            val = node.val
+            if val <= lower or val >= upper:
+                return False
+
+            if not helper(node.right, val, upper):
+                return False
+            if not helper(node.left, lower, val):
+                return False
+            return True
+
+        return helper(root)
+
