36a3676868c1cc376538aea739058dfb5b413540
Sandeep Yadav <sandeepyadav10011995@gmail.com>
2020-06-02 00:00:53 +0530

    Create Balanced Binary Tree.py

---
+++ BST/Balanced Binary Tree.py
@@ -0,0 +1,17 @@
+"""
+Given a binary tree, determine if it is height-balanced.
+For this problem, a height-balanced binary tree is defined as:
+A binary tree in which the left and right subtrees of every node differ in height by no more than 1.
+
+Example 1:
+Given the following tree [3,9,20,null,null,15,7]:
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+Return true.
+
+
+"""
