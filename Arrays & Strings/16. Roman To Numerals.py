4a2b7c86a10a480e31ec8a2b8c84ec1342109a76
Sandeep Yadav <sandeepyadav10011995@gmail.com>
2020-12-30 14:06:16 +0530

    Create 16. Roman To Numerals.py

---
+++ Arrays & Strings/16. Roman To Numerals.py
@@ -0,0 +1,59 @@
+values = {
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000,
+}
+
+values2 = {
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000,
+    "IV": 4,
+    "IX": 9,
+    "XL": 40,
+    "XC": 90,
+    "CD": 400,
+    "CM": 900
+}
+
+class Solution:
+    def romanToInt1(self, s):
+        total = 0
+        i = 0
+        while i < len(s):
+            # If this is the subtractive case.
+            if i + 1 < len(s) and values[s[i]] < values[s[i + 1]]:
+                total += values[s[i + 1]] - values[s[i]]
+                i += 2
+            # Else this is NOT the subtractive case.
+            else:
+                total += values[s[i]]
+                i += 1
+        return total
+
+    def romanToInt2(self, s: str) -> int:
+        total = 0
+        i = 0
+        while i < len(s):
+            # If this is the subtractive case.
+            if i + 1 < len(s) and s[i:i+2] in values2:
+                total += values2[s[i:i+2]]
+                i += 2
+            # Else this is NOT the subtractive case.
+            else:
+                total += values2[s[i]]
+                i += 1
+        return total
+
+s = 'MMCMLXXXIX'
+sol = Solution()
+print(sol.romanToInt1(s))
+print(sol.romanToInt2(s))
