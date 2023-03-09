d4f176ffffcbb015c0e79b59aa1c328c981249a2
Sandeep Yadav <sandeepyadav10011995@gmail.com>
2023-03-09 13:18:19 +0530

    Update 2. SquaringASortedArray.py

--- Pattern-Two Pointers/2. SquaringASortedArray.py
+++ Pattern-Two Pointers/2. SquaringASortedArray.py
@@ -6,7 +6,7 @@ sub-array.
 Problem Statement : Given a sorted array, create a new array containing squares of all the numbers of the input array in
                     the sorted order.
 
-Algo :
+Algo : 
 
 
 Example 1:
